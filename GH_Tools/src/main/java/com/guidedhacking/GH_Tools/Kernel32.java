f9090c39d0a0bd20f1174fae15c496884c154bd9
OpticFusion1 <OpticFusion1@users.noreply.github.com>
2020-07-29 19:03:42 -0400

    Changes Bumps up the version Removes the agent and api jar files Moved GHTools#loadLibrary to Utils#loadLibrary Added https://github.com/D4stiny/JLoadLibrary to GH_Tools, Utils#.inject(processId, dllName)

---
+++ GH_Tools/src/main/java/com/guidedhacking/GH_Tools/Kernel32.java
@@ -0,0 +1,29 @@
+package com.guidedhacking.GH_Tools;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.BaseTSD.DWORD_PTR;
+import com.sun.jna.platform.win32.WinDef.BOOL;
+import com.sun.jna.platform.win32.WinDef.LPVOID;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
+import com.sun.jna.win32.StdCallLibrary;
+
+public interface Kernel32 extends StdCallLibrary {
+
+  public HANDLE OpenProcess(DWORD_PTR dwDesiredAccess, BOOL bInheritHandle, DWORD_PTR dwProcessId);
+
+  public DWORD_PTR GetProcAddress(HANDLE hModule, String lpProcName);
+
+  public LPVOID VirtualAllocEx(HANDLE hProcess, LPVOID lpAddress, int dwSize, DWORD_PTR flAllocationType, DWORD_PTR flProtect);
+
+  public BOOL WriteProcessMemory(HANDLE hProcess, LPVOID lpBaseAddress, Pointer lpBuffer, int nSize, Pointer lpNumberOfBytesWritten);
+
+  public DWORD_PTR CreateRemoteThread(HANDLE hProcess, int lpThreadAttributes, int dwStackSize, DWORD_PTR loadLibraryAddress, LPVOID lpParameter, int dwCreationFlags, int lpThreadId);
+
+  public BOOL CloseHandle(HANDLE hObject);
+  //public HANDLE GetModuleHandleW(WString lpModuleName);
+
+  public int GetLastError();
+
+  public HANDLE GetModuleHandle(String string);
+
+}
